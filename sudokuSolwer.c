15f1ca9c36142a5c887a6d418e9ea36299ddb700
Abhishek Jajoria <abhishek@unicommerce.com>
2013-12-01 19:41:21 +0530

    display of  sudoku in main

--- sudokuSolwer.c
+++ sudokuSolwer.c
@@ -2,8 +2,11 @@
 *Create By Abhishek Jajoria
 */
 #include<stdio.h>
+#define TRUE 1
+#define FALSE 0
 
-int sudoku[9][9]={{0,0,0,9,0,0,7,0,0},
+int sudoku[9][9]={
+		  {0,0,0,9,0,0,7,0,0},
 		  {0,0,0,0,0,0,0,9,5},
 		  {5,0,0,0,8,0,4,3,0},
 		  {9,4,0,0,2,5,0,0,7},
@@ -13,7 +16,25 @@ int sudoku[9][9]={{0,0,0,9,0,0,7,0,0},
 		  {3,9,0,0,0,0,0,0,0},
 		  {0,0,7,0,0,8,0,0,0}
 };
+
 int good();
+int try();
 
 int main(){
+
+	for(int i = 0;i<9;i++){
+	   for(int j=0;j<9;j++){
+		printf("  %d ",sudoku[i][j]);
+	}
+	printf("\n\n");
+     }
+
+ /** if(try==TRUE)
+	for(int i = 0;i<3;i++){
+	   for(int j=0;j<3;j++){
+		printf("  %d  ",sudoku[i][j]);
+	}
+	printf("\n\n");
+     }*/
+		return 0;
 }
