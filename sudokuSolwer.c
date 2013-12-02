bf46a4005c2ecc4b94d60d3a8d20e3241b12052d
Abhishek Jajoria <abhishek@unicommerce.com>
2013-12-02 23:53:31 +0530

    good function updated

--- sudokuSolwer.c
+++ sudokuSolwer.c
@@ -28,7 +28,8 @@ int main(){
 	}
 	printf("\n\n");
      }
-
+int check =good();
+printf("\n %d ",check);
  /** if(try==TRUE)
 	for(int i = 0;i<3;i++){
 	   for(int j=0;j<3;j++){
@@ -38,3 +39,36 @@ int main(){
      }*/
 		return 0;
 }
+
+int good(){
+
+
+	/**	Check For Horizontal	*/
+
+	for(int i = 0;i<9;i++){
+	int tempArray[]={0,0,0,0,0,0,0,0,0};
+	   for(int j=0;j<9;j++){
+		if(tempArray[sudoku[j][i]-1]==sudoku[j][i]&&(sudoku[j][i]!=0))
+			return FALSE;
+		else if(sudoku[j][i]!=0){
+		tempArray[sudoku[j][i]-1] = sudoku[j][i];
+		}
+	}
+
+}
+
+	/***	Check For Vertical 	***/
+
+	for(int i = 0;i<9;i++){
+	int tempArray[9]={0,0,0,0,0,0,0,0,0};
+	   for(int j=0;j<9;j++){
+		if(tempArray[sudoku[j][i]-1]==sudoku[j][i]&&(sudoku[j][i]!=0))
+			return FALSE;
+		else if(sudoku[j][i]!=0){
+		tempArray[sudoku[j][i]-1] = sudoku[j][i];
+		}
+	}
+}
+	return TRUE;
+}
+
